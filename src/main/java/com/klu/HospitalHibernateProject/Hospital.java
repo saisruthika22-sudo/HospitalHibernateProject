5441e15036ea2e67eebcb3acba9fecd0e65e003d
saisruthika22-sudo <saisruthika22@gmail.com>
2026-03-11 11:28:46 +0530

    Update Hospital.java

--- src/main/java/com/klu/HospitalHibernateProject/Hospital.java
+++ src/main/java/com/klu/HospitalHibernateProject/Hospital.java
@@ -1,5 +1,55 @@
-package com.klu.HospitalHibernateProject;
+package com.klef.fsad.exam;
 
-public class Hospital {
+import jakarta.persistence.*;
+import java.util.Date;
 
+@Entity
+@Table(name="hospital")
+public class Hospital
+{
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    
+    private String name;
+    private String description;
+    
+    @Temporal(TemporalType.DATE)
+    private Date date;
+    
+    private String status;
+
+    public Hospital() {}
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
 }
