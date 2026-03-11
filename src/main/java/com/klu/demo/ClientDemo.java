53a8a889c87481b868dcd11da7b8d3491c815a0c
saisruthika22-sudo <saisruthika22@gmail.com>
2026-03-11 11:28:19 +0530

    Update ClientDemo.java

--- src/main/java/com/klu/demo/ClientDemo.java
+++ src/main/java/com/klu/demo/ClientDemo.java
@@ -1,5 +1,36 @@
-package com.klu.demo;
+package com.klef.fsad.exam;
 
-public class ClientDemo {
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+import java.util.Date;
 
+public class ClientDemo
+{
+    public static void main(String[] args)
+    {
+        SessionFactory sf = new Configuration().configure().buildSessionFactory();
+        Session session = sf.openSession();
+        session.beginTransaction();
+
+        Hospital h = new Hospital();
+        h.setName("Apollo");
+        h.setDescription("Multi Speciality Hospital");
+        h.setDate(new Date());
+        h.setStatus("Active");
+
+        session.save(h);
+
+        session.getTransaction().commit();
+
+        int id = h.getId();
+        Hospital data = session.get(Hospital.class, id);
+
+        System.out.println(data.getName());
+        System.out.println(data.getDescription());
+        System.out.println(data.getStatus());
+
+        session.close();
+        sf.close();
+    }
 }
